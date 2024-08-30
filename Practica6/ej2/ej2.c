28173a91ffdb861d1f40e82f25428d44567648a7
blanusse <blanusse@itba.edu.ar>
2024-08-30 14:11:18 -0300

    Empiezo P6

---
+++ Practica6/ej2/ej2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "../../Lib/getnum.h"
+
+extern int num=0, den=0;
+
+int divisorComunMaximo(int a, int b) {
+    int divisor = 1;
+    for(int i=1; i <= (a<b? a : b); i++) {
+        if ((a % i == 0) && (b%i == 0) )
+            divisor=i;
+    }
+    return divisor;
+}
+void simplificar(int a, int b) {
+    if (b==0)
+        a=0, b=0;
+    int mcd = divisorComunMaximo(a, b);
+    num /=mcd;
+    den /=mcd;
+}
+
+int menuOpciones (void) {
+    int opcion = 0;
+    do
+        opcion = getint("1)Simplificar una funcion\n"
+                        "2)Sumar dos fracciones\n3)Terminar la ejecucion\nElija una opcion: ");
+    while (opcion!= 1 && opcion != 2 && opcion != 3);
+    return opcion;
+}
+
+void sumaFracciones (int a, int b, int c, int d) {
+    den = b*d;
+    num = (a*d+b*c);
+    simplificar(num, den);
+}
+
+
+
+int main() {
+    int opcion = menuOpciones();
+    int a, b, c, d;
+    switch (opcion) {
+        case 1:
+            num = getint("Ingrese el numerador: ");
+            den = getint("Ingrese el denominador: ");
+            simplificar(num, den);
+            printf("%d/%d", num, den);
+        break;
+        case 2:
+            a = getint("Ingrese un numerador: ");
+            b =getint("Ingrese un denominador: ");
+            c = getint("Ingrese otro numerador: ");
+            d =getint("Ingrese un denominador: ");
+            sumaFracciones(a,b,c,d);
+            printf("%d/%d", num, den);
+        break;
+        default:
+            return 0;
+    }
+}
